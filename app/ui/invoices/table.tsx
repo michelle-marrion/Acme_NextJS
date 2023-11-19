6dfeb6c5ce1fb7b7caac392ada1f51824a4900d4
Michelle-Marrion <ngamimarrion@gmail.com>
2023-11-19 02:00:01 +0100

    changes

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt=""
+                        alt="Screenshots of the dashboard project showing desktop and mobile versions"
                       />
                       <p>{invoice.name}</p>
                     </div>
